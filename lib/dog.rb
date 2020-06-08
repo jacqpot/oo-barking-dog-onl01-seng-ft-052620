4c4c83e8b59dc1c5ba9404521fc3adc8dc12d79d
Jacques Potgieter <potgieter.g.jacques@gmail.com>
2020-06-08 17:18:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,59 @@
-class Dog 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Dog 
   
-  def name=(dog_name)
-    @this_dogs_name = dog_name
+#   def name=(dog_name)
+#     @this_dogs_name = dog_name
     
-  end
+#   end
   
-  def name
-    @this_dogs_name
-  end
+#   def name
+#     @this_dogs_name
+#   end
   
-  def bark
-    puts "woof!"
-  end
+#   def bark
+#     puts "woof!"
+#   end
   
-end
+# end
